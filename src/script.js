4c8038f40a365879f540dbddbdd84f4fc821c290
mariannebarbin-png <mariannebarbin@gmail.com>
2025-10-17 21:06:33 +0800

    edit1

--- src/script.js
+++ src/script.js
@@ -10,8 +10,9 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const colorTexture = textureLoader.load('./textures/door/color.jpg')
-
+const colorTexture = textureLoader.load(
+  './textures/door/color.jpg'
+)
 // Configure texture properties
 colorTexture.repeat.x = 2
 colorTexture.repeat.y = 3
